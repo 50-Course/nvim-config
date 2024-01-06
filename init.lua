b50d723e94e41d1a27840c5cfab36ebafe872b3e
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-01-06 17:00:17 +0100

    Update config

--- init.lua
+++ init.lua
@@ -14,11 +14,17 @@ require('packer').startup(function(use)
 	-- for the packer plugin itself
 	use 'wbthomason/packer.nvim'
 
+	-- Pass me the harpoon for swift buffer navigation
+	use 'ThePrimeagen/harpoon.nvim'
+
 
 end)
 
 
 -- ======================== GLOBAL CONFIGURATION ========================
+--
+-- Vim options are settings that set the behaviour of a buffer or window.
+-- Use: `:h options` or `:h options-list` for more infomation.
 local options = {
 	number = true,
 	relativenumber = true,
@@ -43,3 +49,14 @@ for conf, val in pairs(options) do
 	vim.opt[conf] = val
 end
 
+-- ======================== USER/AUTOCOMMANDS ========================
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local highlight_text = augroup('TextHighlight', {clear = true })
+autocmd('TextYankPost', {
+	group = highlight_text,
+	command = function()
+		vim.highlight.on_yank()
+	end
+})
