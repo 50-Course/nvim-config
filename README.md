6f5060cd11434732990366a5e3786e95c9bfbd6b
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-01-06 10:54:33 +0100

    :brick:

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Moving from IDE-based editors to text based ediotrs, this living repository contains configuration scripts
 for y development workflow.
+
--- init.lua
+++ init.lua
@@ -1,1 +1,33 @@
-print 'Hello world'
+-- I am using Packer as my plugin manager
+
+vim.cmd [[packadd packer.nvim ]]
+
+-- ======================== PLUGINS MANAGEMENT ========================
+require('packer').setup(function(use)
+
+	-- Packer-in-Packer (PIP) to manage itself
+	--
+	-- This option allows to manage version updates
+	-- for the packer plugin itself
+	use 'wbthomason/packer.nvim'
+
+
+end)
+
+
+-- ======================== GLOBAL CONFIGURATION ========================
+local options = {
+	number = true
+	relativenumber = true
+
+	termguicolors = true
+	smartindent = true
+	autoindent = true
+	tabspace = 4
+	smarttabspace = 4
+}
+
+for conf, val in ipairs(options) do
+	vim.opt[conf] = val
+end
+
