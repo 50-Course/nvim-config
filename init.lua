ceb02927eaba63b24170e7dc83e9e2613a815ff0
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-01-07 09:00:22 +0100

    additional keybinds for sweet navigation and granular control

--- init.lua
+++ init.lua
@@ -47,6 +47,10 @@ require('packer').startup(function(use)
     -- File explorer sucks, just fuzzy bro@
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.5' }
 
+    -- You never know when i'd be dealing with Web Dev
+    --
+    -- 
+
 end)
 
 
@@ -83,6 +87,12 @@ local options = {
 for conf, val in pairs(options) do
 	vim.opt[conf] = val
 end
+
+-- Just ignore `node_modules` and `.git`. Seriously, its the worst place
+-- to be in the universe
+-- vim.opt.wildignore += '**/.git/*'
+-- vim.opt.wildignore += '**/node_modules/*'
+
 
 --- Disable VIM defaults
 -- Nobody likes the top banner on NetRW -- I don't!
@@ -132,11 +142,12 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
 
--- Use system clipboards instead of Vim's built-in
+-- Use system clipboards instead of Vim's built-in clipboard
 keymap.set({'n', 'v'}, '<leader>y', '"+Y')
 keymap.set({'n', 'v'}, '<leader>d', '"_d')
-keymap.set('n', '<leader>p', '"dP')
+keymap.set('n', '<leader>p', '"+P')
 
+-- Navigation is better with `project-view`
 keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Better way to jump out of modes
@@ -147,6 +158,18 @@ keymap.set('n', 'Q', '<nop>')
 --- Git bindings
 keymap.set('n', '<leader>gs', function() vim.cmd [[ Git ]] end)
 
+-- Make text selection move up and down in selection mode
+keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+
+-- Use <Ctrl-x'> to make a bash script executable
+keymap.set("n", "<C-x>", "<cmd>silent !chmod +x %<cr>")
+
+-- Keep cursor at the middle of the buffer at all times
+-- ..when Ctrl+d is pressed to scroll to the bottom of the page
+-- ..and when the reverse is done with Ctrl+u
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
 
 
 -- ======================== PLUGINS CONFIGURATION ========================
