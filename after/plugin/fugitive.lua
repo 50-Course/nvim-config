a0ac4538f3a0e6def828d64df4f29093a93470e8
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-02-23 13:14:24 +0100

    feat(git): add telescope to switch branches when working on large repositories

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,7 +2,7 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 local git_keymaps = function()
-    local opts = { noremap = true, silent = true}
+    local opts = { noremap = true, silent = true }
 
     vim.keymap.set("n", "<leader>gs", ":Git<CR>", opts)
     -- Git push
@@ -11,10 +11,10 @@ local git_keymaps = function()
     vim.keymap.set("n", "<leader>P", function()
         -- if there are changes, stash them, pull, then pop the stash
         -- otherwise just pull
-        vim.cmd.Git('stash')
+        vim.cmd.Git("stash")
         -- rebase and display the log in a split
-        vim.cmd [[Git pull origin --rebase | copen]]
-        vim.cmd.Git('stash pop')
+        vim.cmd([[Git pull origin --rebase | copen]])
+        vim.cmd.Git("stash pop")
     end, opts)
 
     -- Git push to named branch
@@ -23,13 +23,27 @@ local git_keymaps = function()
         vim.cmd.Git({ "push", "-u", "origin", branch })
     end, opts)
 
+    vim.keymap.set('n', '<leader>gco', function ()
+        local _, telescope_builtin = pcall(require, "telescope.builtin")
+        telescope_builtin.git_branches({
+            attach_mappings = function(_, map)
+                map('i', '<CR>', function(bufnr)
+                    local entry = require('telescope.actions.state').get_selected_entry()
+                    require('telescope.actions').close(bufnr)
+                    vim.cmd('Git checkout ' .. entry.value)
+                end)
+                return true
+            end
+        })
+    end, opts)
+
     -- add and push tags interactively
     -- TODO: implement this
     vim.keymap.set("n", "<leader>ptt", function()
-        local _, telescope_builtin = pcall(require, "telescope.builtin")
+        -- local _, telescope_builtin = pcall(require, "telescope.builtin")
 
         -- UPDATE: this is not working as expected, I am able to see the list of tags,
-        -- but I am not able to select them and push them - I am thnking of using 
+        -- but I am not able to select them and push them - I am thnking of using
         -- fzf and piping the output to git push
 
         -- local tag_name = vim.fn.input("Tag name: ")
@@ -44,7 +58,6 @@ local git_keymaps = function()
         -- -- if tag exists, push it
         -- print(vim.inspect(tags))
         --
-
     end, opts)
 end
 
