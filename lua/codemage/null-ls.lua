38e1285f43f041fc1fb613a54a7db2166e7f9cde
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-03-02 01:32:10 +0100

    format buffer on save

--- lua/codemage/null-ls.lua
+++ lua/codemage/null-ls.lua
@@ -1,14 +1,10 @@
 local _, null_ls = pcall(require, "null-ls")
 
 local sources = {
-    null_ls.builtins.diagnostics.eslint.with({
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-    }),
     null_ls.builtins.diagnostics.commitlint,
-    null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.code_actions.gitsigns,
     null_ls.builtins.formatting.prettierd.with({
-	filetypes = { "html", "json", "yaml", "markdown" },
+	filetypes = { "html", "json", "yaml", "markdown", "css", "scss", "less", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
     }),
     null_ls.builtins.formatting.clang_format.with({
 	filetypes = { "c", "cpp", "objc", "objcpp" },
@@ -23,4 +19,14 @@ local sources = {
     null_ls.builtins.formatting.shfmt,
 }
 
-null_ls.setup({ sources = sources })
+
+local attach_to_lsp = function(client)
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd [[augroup Format]]
+        vim.cmd [[autocmd! * <buffer>]]
+        vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+        vim.cmd [[augroup END]]
+    end
+end
+
+null_ls.setup({ debug = false, sources = sources, on_attach = attach_to_lsp})
