8180f2df2e93a0fd32def65049df691b83425b73
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-07-30 04:15:58 +0100

    Add `nvim-java` for java and spring projects

---
+++ lua/codemage/mason.lua
@@ -0,0 +1,24 @@
+-- Nvim java uses a fork of Mason registry so we ahve to setup the two
+--
+local registries = {
+    "github:nvim-java/mason-registry",
+    "github:mason-org/mason-registry",
+}
+
+-- setup mason
+require("mason").setup({
+    registries = registries,
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+        },
+    },
+})
+
+-- print("Loaded custom Mason config!")
+
+-- setup nvim java and lspconfig
+require("java").setup({})
+require("lspconfig").jdtls.setup({})
